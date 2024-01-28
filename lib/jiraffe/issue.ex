dff96afc10f84dce1f2c6221eea0d995389246b5
Yuriy Marchenko <dorialan@gmail.com>
2024-01-28 14:35:53 +0100

    Add `Issue.new` function to convert map to Jira.Issue struct

--- lib/jiraffe/issue.ex
+++ lib/jiraffe/issue.ex
@@ -38,6 +38,29 @@ defmodule Jiraffe.Issue do
   @type error() :: {:error, Error.t()}
 
   @doc """
+  Converts a map (received from Jira API) to `Jira.Issue` struct.
+  """
+  def new(body) do
+    %__MODULE__{
+      expand: body["expand"],
+      id: body["id"],
+      self: body["self"],
+      key: body["key"],
+      rendered_fields: Map.get(body, "renderedFields", %{}),
+      properties: Map.get(body, "properties", %{}),
+      names: Map.get(body, "names", %{}),
+      schema: Map.get(body, "schema", %{}),
+      transitions: Map.get(body, "transitions", []),
+      operations: Map.get(body, "operations", []),
+      editmeta: Map.get(body, "editmeta", %{}),
+      changelog: body["changelog"],
+      versioned_representations: body["versionedRepresentations"],
+      fields_to_include: body["fieldsToInclude"],
+      fields: Map.get(body, "fields", %{})
+    }
+  end
+
+  @doc """
   Get an issue by ID or key
 
   [Reference](https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-issues/#api-rest-api-2-issue-issueidorkey-get)
@@ -50,24 +73,7 @@ defmodule Jiraffe.Issue do
   def get(client, id_or_key, params \\ []) do
     case Jiraffe.get(client, "/rest/api/2/issue/" <> id_or_key, query: params) do
       {:ok, %{status: 200, body: body}} ->
-        {:ok,
-         %__MODULE__{
-           expand: body["expand"],
-           id: body["id"],
-           self: body["self"],
-           key: body["key"],
-           rendered_fields: Map.get(body, "renderedFields", %{}),
-           properties: Map.get(body, "properties", %{}),
-           names: Map.get(body, "names", %{}),
-           schema: Map.get(body, "schema", %{}),
-           transitions: Map.get(body, "transitions", []),
-           operations: Map.get(body, "operations", []),
-           editmeta: Map.get(body, "editmeta", %{}),
-           changelog: body["changelog"],
-           versioned_representations: body["versionedRepresentations"],
-           fields_to_include: body["fieldsToInclude"],
-           fields: Map.get(body, "fields", %{})
-         }}
+        {:ok, new(body)}
 
       {:ok, result} ->
         {:error, Error.new(:unexpected_status, result)}
